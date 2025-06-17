ab7de0a4f63f7b5e741d1d8991c9ce47aac95f27
RodrigoKuerten <rodrigokuerten31@gmail.com>
2025-06-17 11:54:37 -0300

    Contact.java

---
+++ Contact.java
@@ -0,0 +1,44 @@
+package projetos.Agenda;
+
+public class Contact {
+	private String name;
+	private String number;
+	private String address;
+
+	public Contact(String name, String number, String address) {
+		this.name = name;
+		this.number = number;
+		this.address = address;
+	}
+	public void ContactCleaner() {
+	    this.name = "indefinido";
+	    this.number = "indefinido";
+	    this.address = "indefinido";
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String  getAddress() {
+		return address;
+	}
+
+}
--- main.java
+++ main.java
@@ -7,21 +7,23 @@ import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
+		List<Contact> infoContact = new ArrayList();
+		Contact contact = new Contact("indefinido", "indefinido", "indefinido");
 		Scanner scanner = new Scanner(System.in);
-		Contacts contatos = new Contacts();
-		List<String> listNames = new ArrayList<>();
-		List<String> listNumbers = new ArrayList<>();
 
 		int menu = 0;
-		int peoples = 0;
+		int index = 0;
 		int comunication = 0;
 		String name = null;
 		String number = null;
+		String address = null;
 
 		while (menu <= 1 || menu >= 6) {
 			System.out.println("Lista De Contatos:");
 			System.out.println();
-			contatos.listContacts(peoples);
+			for (index = 0; index <= (infoContact.size() - 1); index++) {
+				System.out.println(index + 1 + " -- " + infoContact.get(index).getName() + " -- " + infoContact.get(index).getNumber() + " -- " + infoContact.get(index).getAddress());
+			}
 			System.out.println();
 			System.out.println("'1' - Adicionar contato:");
 			System.out.println("'2' - Editar contato:");
@@ -31,23 +33,126 @@ public class main {
 			menu = scanner.nextInt();
 
 			switch (menu) {
+
 			case 1:
-				contatos.addContact(name,number);
+				scanner.nextLine();
+				System.out.println("Qual é o nome do seu contato?");
+				name = scanner.nextLine();
+				contact.setName(name);
+				System.out.println();
+
+				System.out.println("Qual é o número desse contato?");
+				number = scanner.nextLine();
+				System.out.println();
+
+				contact.setNumber(number);
+				System.out.println("Qual é o endereço da pessoa?\n");
+				address = scanner.nextLine();
+				contact.setAddress(address);
+				System.out.println();
+
+				infoContact.add(new Contact(contact.getName(), contact.getNumber(), contact.getAddress()));
 				menu = -1;
+				System.out.println();
 				break;
 			case 2:
-				contatos.changeDados(menu, peoples, name, number);
+
+				System.out.println();
+				if (infoContact.size() > 0) {
+					System.out.println("Qual contato você gostaria de editar?");
+					System.out.println();
+					for (index = 0; index <= (infoContact.size() - 1); index++) {
+						System.out.println(index + 1 + " -- " + infoContact.get(index).getName() + " -- " + infoContact.get(index).getNumber() + " -- " + infoContact.get(index).getAddress());
+					}
+					index = scanner.nextInt();
+					index--;
+					menu = -1;
+
+					while (menu < 1 || menu > 4) {
+						System.out.println("O que você quer editar nesse contato?");
+						System.out.println();
+						System.out.println("1 - Nome");
+						System.out.println("2 - Número");
+						System.out.println("3 - Endereço");
+						System.out.println("4 - Sair");
+						menu = scanner.nextInt();
+					}
+					switch (menu) {
+					case 1:
+						scanner.nextLine();
+						System.out.println("Qual o nome você gostaria de coloca-lo para alterar?");
+						name = scanner.nextLine();
+						Contact c = infoContact.get(index);
+						c.setName(name);
+
+						break;
+					case 2:
+						scanner.nextLine();
+						System.out.println("Qual é o número que você quer colocar para edita-lo?");
+						number = scanner.nextLine();
+						Contact d = infoContact.get(index);
+						d.setNumber(number);
+
+						break;
+					case 3:
+						scanner.nextLine();
+						System.out.println("Qual é o endereço atual desse contato?");
+						address = scanner.nextLine();
+						Contact e = infoContact.get(index);
+						e.setNumber(address);
+						break;
+					case 4:
+						System.out.println("Retornando para a página principal...");
+						break;
+					}
+				} else {
+					System.out.println("Você não possui contatos, adicione pelo menos 1 para acessar essa pagina!");
+				}
 				menu = -1;
+				System.out.println();
 				break;
-			case 3:
-				contatos.comunication(peoples,comunication);
-				menu = -1;
-				break;
+
 			case 4:
-				contatos.deleteContact(peoples);
+				if (infoContact.isEmpty()) {
+					System.out.println("A lista está vazia!");
+				} else {
+					for (index = 0; index <= (infoContact.size() - 1); index++) {
+						System.out.println(index + 1 + " -- " + infoContact.get(index).getName() + " -- "
+								+ infoContact.get(index).getNumber() + " -- " + infoContact.get(index).getAddress());
+					}
+					System.out.println();
+					while (index < 0 || index > (infoContact.size() - 1)) {
+						System.out.println("Selecione o contato que deseja excluir pelo número!");
+						index = scanner.nextInt();
+						index--;
+					}
+					infoContact.remove(index);
+					System.out.println("Contato removido com sucesso!");
+				}
 				menu = -1;
+				System.out.println();
+				break;
+
+			case 3:
+				if (infoContact.size() > 0) {
+					System.out.println("Seus contatos:");
+					System.out.println();
+					for (index = 0; index <= (infoContact.size() - 1); index++) {
+						System.out.println(index + 1 + " -- " + infoContact.get(index).getName() + " -- "
+								+ infoContact.get(index).getNumber() + " -- " + infoContact.get(index).getAddress());
+					}
+					System.out.println("Qual contato você deseja se comunicar?");
+					comunication = scanner.nextInt();
+					if (comunication <= 0 || comunication >= (infoContact.size() - 1)) {
+						System.out.println("Olá " + infoContact.get(comunication - 1).getName() + " como está? Tudo bem?");
+					}
+				}
+				menu = -1;
+				System.out.println();
 				break;
 			}
+			
+
 		}
 		System.out.println("Obrigado por usar nosso aplicativo de agenda de contatos, até mais!");
 	}
